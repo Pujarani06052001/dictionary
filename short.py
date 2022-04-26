594e293a11745377f176f05a55e830cb9fe3c456
Pujarani06052001 <103930803+Pujarani06052001@users.noreply.github.com>
2022-04-26 10:14:23 +0530

    Add files via upload

---
+++ short.py
@@ -0,0 +1,25 @@
+# a= [11, 3, 7, 5, 2]
+# a.sort()
+# print(a)
+
+
+# vowels = ['e', 'a', 'u', 'o', 'i']
+# vowels.sort()
+# print('Sorted list:', vowels)
+
+
+
+# dict = {6:'George' ,2:'John' ,1:'Potter' ,9:'Micheal' ,7:'Robert' ,8:'Gayle' }  
+# b = sorted(dict.keys())
+# print("Sorted keys",b)    
+# c = sorted(dict.items())
+# print("Sorted Values",c)  
+
+
+# a = {1:2 ,2:1 ,4:3 ,3:4 ,6:5 ,5:6 }
+# print(sorted(a.values()))
+
+
+# a = {1:2 ,2:1 ,4:3 ,3:4 ,6:5 ,5:6 }
+# print(sorted(a.keys()))
+
